645d463065177e68f528b768a9260e60d6f469d8
Jacob Hummer <jcbhmr@outlook.com>
2023-08-14 00:58:09 -0500

    try explicit name

--- test.sh
+++ test.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 set -ex
 feature_dir=$PWD
+feature_id=$(jq -r .id devcontainer-feature.json)
 pushd "$(mktemp -d)"
 mkdir -p .devcontainer
 cat <<EOF >.devcontainer/devcontainer.json
 {
-  "image": "${1:-'mcr.microsoft.com/devcontainers/base:debian'}",
+  "image": "${1:-'mcr.microsoft.com/devcontainers/base'}",
   "features": {
-    "./feature": {}
+    "./$feature_id": ${2:-'{}'}
   }
 }
 EOF
-ln -s "$feature_dir" "$PWD/.devcontainer/feature"
+ln -s "$feature_dir" "$PWD/.devcontainer/$feature_id"
 devcontainer up --workspace-folder .
 devcontainer exec --workspace-folder . cmake --version
 echo "TODO: Kill container after test"
