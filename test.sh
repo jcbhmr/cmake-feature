7e0080fe7a352846c6778c5a64db9ac1a8996567
Jacob Hummer <jcbhmr@outlook.com>
2023-08-14 01:06:57 -0500

    try options?

--- test.sh
+++ test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -ex
 feature_dir=$PWD
-feature_id=$(jq -r .id devcontainer-feature.json)
 temp_dir=$(mktemp -d)
 trap 'rm -rf "$temp_dir"' SIGINT SIGTERM ERR EXIT
 pushd "$temp_dir"
@@ -10,12 +9,12 @@ cat <<EOF >.devcontainer/devcontainer.json
 {
   "image": "${1:-'mcr.microsoft.com/devcontainers/base'}",
   "features": {
-    "./$feature_id": ${2:-'{}'}
+    "./feature": ${2:-'{}'}
   }
 }
 EOF
-rsync -av --exclude .git "$feature_dir" "$PWD/.devcontainer/$feature_id"
-tree
+rsync -av --exclude .git "$feature_dir" "$PWD/.devcontainer/feature"
+tree -a
 devcontainer up --workspace-folder .
 devcontainer exec --workspace-folder . cmake --version
 echo "TODO: Kill container after test"
