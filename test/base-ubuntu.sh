478c4917f47568278e78c34a84bbaf414e3ae440
Jacob Hummer <jcbhmr@outlook.com>
2023-08-14 00:31:32 -0500

    changeup tests

---
+++ test/base-ubuntu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -ex
+id=$(basename "$BASH_SOURCE")
+mkdir -p .cache/$id/.devcontainer
+cat <<EOF >.cache/$id/.devcontainer/devcontainer.json
+{
+  "image": "mcr.microsoft.com/devcontainers/base:ubuntu",
+  "features": {
+    "../..": {}
+  }
+}
+EOF
+devcontainer up --workspace-folder .cache/$id
+devcontainer exec --workspace-folder .cache/$id cmake --version
+echo "TODO: Kill container after test"
